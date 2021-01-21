c9f2a2df5135e4a016e05a78ebdac679d76997f1
Alex <alexsandro.sousa12366@gmail.com>
2021-01-21 16:55:59 -0300

    fixed 01

--- js/main.js
+++ js/main.js
@@ -248,13 +248,13 @@ function bodyScrollingToggle(){
 
 // Hide all sections except active
 
-(() => {
+// (() => {
 
-    const sections = document.querySelectorAll(".section");
-    sections.forEach((section) => {
-        if(!section.classList.contains("active")){
-            section.classList.add("hide");
-        }
-    })
+//     const sections = document.querySelectorAll(".section");
+//     sections.forEach((section) => {
+//         if(!section.classList.contains("active")){
+//             section.classList.add("hide");
+//         }
+//     })
 
-})();
+// })();
